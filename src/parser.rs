3979c4c3172fa3982380e71073262484ef6acf6d
kallehed <kallehed@gmail.com>
2023-08-25 22:29:44 +0200

    add correct multiplication priority rules, refactor parsing a lot

--- src/parser.rs
+++ src/parser.rs
@@ -10,16 +10,22 @@ type ASTBox<T> = Box<T>;
 type ASTVec<T> = Vec<T>;
 type ASTBody = ASTVec<ASTStatement>;
 
+#[derive(Debug, Copy, Clone)]
+pub enum BinaryOp {
+    Argument,
+    Add,
+    Sub,
+    Multiply,
+    Equals,
+}
+
 #[derive(Debug)]
 pub enum ASTExpr {
     Number(i64),
     String(&'static str),
     VarName(VariableNameIdx),
     FunctionCall(FunctionNameIdx, Option<ASTBox<ASTExpr>>),
-    Argument(ASTBox<ASTExpr>, ASTBox<ASTExpr>),
-    Equals(ASTBox<ASTExpr>, ASTBox<ASTExpr>),
-    Add(ASTBox<ASTExpr>, ASTBox<ASTExpr>),
-    Multiply(ASTBox<ASTExpr>, ASTBox<ASTExpr>),
+    Binary(BinaryOp, ASTBox<ASTExpr>, ASTBox<ASTExpr>),
     Neg(ASTBox<ASTExpr>),
 }
 
@@ -77,78 +83,99 @@ pub fn parse_tokens(tokens: &[Token]) {
     }
 }
 
+/// parse expression formed of tokens
 fn parse_tokens_expression(tokens: &mut TokenIter) -> Option<ASTExpr> {
-    let token = tokens.next().unwrap();
-    let first_part = match token {
-        &Token::Number(num) => {
-            // we got a number, now we must find an operator
-            ASTExpr::Number(num)
-        }
-        &Token::VariableName(var_name) => ASTExpr::VarName(var_name),
-        &Token::FunctionName(function_name) => {
-            match tokens.next().unwrap() {
-                Token::Keyword(Keyword::StartParen) => {
-                    // we have a function call, get the expression inside, and this will be the
-                    // first part
-                    ASTExpr::FunctionCall(
-                        function_name,
-                        parse_tokens_expression(tokens).map(|x| ASTBox::new(x)),
-                    )
-                }
-                bad => panic!("wrong token after function name: {:?}", bad),
+    fn parse_value(tokens: &mut TokenIter) -> Option<ASTExpr> {
+        match tokens.next().unwrap() {
+            &Token::Number(num) => {
+                // we got a number, now we must find an operator
+                Some(ASTExpr::Number(num))
             }
+            &Token::VariableName(var_name) => Some(ASTExpr::VarName(var_name)),
+            &Token::FunctionName(function_name) => {
+                match tokens.next().unwrap() {
+                    Token::Keyword(Keyword::StartParen) => {
+                        // we have a function call, get the expression inside, and this will be the
+                        // first part
+                        Some(ASTExpr::FunctionCall(
+                            function_name,
+                            parse_tokens_expression(tokens).map(|x| ASTBox::new(x)),
+                        ))
+                    }
+                    bad => panic!("wrong token after function name: {:?}", bad),
+                }
+            }
+            Token::String(st) => Some(ASTExpr::String(st)),
+            Token::NewLine => {
+                // nothing here,
+                return None;
+            }
+            bad => panic!("bad expression: {:?}", bad),
         }
-        Token::String(st) => {
-            ASTExpr::String(st)
-        }
-        Token::NewLine => {
-            // nothing here,
-            return None;
-        }
-        bad => panic!("bad expression: {:?}", bad),
+    }
+
+    let first_part = match parse_value(tokens) {
+        Some(v) => v,
+        None => return None,
     };
-    match tokens.next().unwrap() {
-        Token::Keyword(Keyword::Plus) => {
-            let ast_expr = ASTExpr::Add(
-                ASTBox::new(first_part),
-                ASTBox::new(
-                    parse_tokens_expression(tokens).expect("Must be more after a plus sign!"),
-                ),
-            );
-            return Some(ast_expr);
+
+    fn parse_oper(tokens: &mut TokenIter) -> Option<BinaryOp> {
+        match tokens.next().unwrap() {
+            Token::Keyword(Keyword::Plus) => {
+                return Some(BinaryOp::Add);
+            }
+            Token::Keyword(Keyword::Minus) => {
+                return Some(BinaryOp::Sub);
+            }
+            Token::Keyword(Keyword::Multiply) => {
+                return Some(BinaryOp::Multiply);
+            }
+            Token::Keyword(Keyword::Comma) => {
+                return Some(BinaryOp::Argument);
+            }
+            Token::NewLine | Token::Keyword(Keyword::EndParen) => {
+                // nothing more
+                return None;
+            }
+            bad => panic!("Bad token after first part: {:?}, ", bad),
         }
-        Token::Keyword(Keyword::Minus) => {
-            let ast_expr = ASTExpr::Add(
-                ASTBox::new(first_part),
-                ASTBox::new(ASTExpr::Neg(ASTBox::new(
-                    parse_tokens_expression(tokens).expect("Must be more after minus sign!"),
-                ))),
-            );
-            return Some(ast_expr);
-        }
-        Token::Keyword(Keyword::Multiply) => {
-            let ast_expr = ASTExpr::Multiply(
-                ASTBox::new(first_part),
-                ASTBox::new(
-                    parse_tokens_expression(tokens).expect("Must be more after multiply sign!"),
-                ),
-            );
-            return Some(ast_expr);
-        }
-        Token::NewLine | Token::Keyword(Keyword::EndParen) => {
-            // nothing more
-            return Some(first_part);
-        }
-        Token::Keyword(Keyword::Comma) => {
-            let ast_expr = ASTExpr::Argument(
-                ASTBox::new(first_part),
-                ASTBox::new(
-                    parse_tokens_expression(tokens)
-                        .expect("Nothing after comma(,) in function call!"),
-                ),
-            );
-            return Some(ast_expr);
-        }
-        bad => panic!("Bad token after first part: {:?}, first_part: {:?}", bad, first_part),
+    }
+    match parse_oper(tokens) {
+        Some(op) => match op {
+            BinaryOp::Multiply => {
+                let next_value = parse_value(tokens).expect("no value after operator * !");
+                let next_oper = parse_oper(tokens);
+                match next_oper {
+                    Some(op2) => Some(ASTExpr::Binary(
+                        op2,
+                        ASTBox::new(ASTExpr::Binary(
+                            BinaryOp::Multiply,
+                            ASTBox::new(first_part),
+                            ASTBox::new(next_value),
+                        )),
+                        ASTBox::new(
+                            parse_tokens_expression(tokens)
+                                .expect("Must be more after an operator!"),
+                        ),
+                    )),
+                    None => Some(ASTExpr::Binary(
+                        BinaryOp::Multiply,
+                        ASTBox::new(first_part),
+                        ASTBox::new(next_value),
+                    )),
+                }
+            }
+            _ => {
+                let a = Some(ASTExpr::Binary(
+                    op,
+                    ASTBox::new(first_part),
+                    ASTBox::new(
+                        parse_tokens_expression(tokens).expect("Must be more after an operator!"),
+                    ),
+                ));
+                a
+            }
+        },
+        None => Some(first_part),
     }
 }
